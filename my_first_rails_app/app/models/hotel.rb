3c8922e58e06a78a96fd600e946cfe6fbece0258
faizm10 <faizmustansar10@gmail.com>
2026-03-02 23:42:37 -0500

    clean

--- my_first_rails_app/app/models/hotel.rb
+++
@@ -1,8 +0,0 @@
-class Hotel < ApplicationRecord
-  belongs_to :trip
-  
-  validates :name, presence: true
-  validates :url, presence: true, format: { with: URI::regexp, message: "must be a valid URL" }
-  validates :price, presence: true, numericality: { greater_than: 0 }
-  validates :currency, presence: true, length: { is: 3 }
-end
